f2e351787a2e118f466c30cadc151b3430ad2f36
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-05-31 21:17:13 -0500

    Add ShellCheck directives to TCS_InstallTool-v2.sh

--- TCS_InstallTool-v2.sh
+++ TCS_InstallTool-v2.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# ShellCheck: Disable SC1090
+# shellcheck source=/dev/null
 # shellcheck disable=SC2034  # Unused variables left for readability
 
 ################################################################################################################
